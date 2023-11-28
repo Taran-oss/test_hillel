e11851fa2cff86e73d67d1362f572fdd1ee04d19
TaranD <dmytro.taran@techtellent.io>
2023-11-28 12:32:50 +0200

    init

---
+++ srs/practics.js
@@ -0,0 +1,52 @@
+// Number
+number = 12;
+
+console.log(typeof number)
+
+// String
+let string = "Text";
+
+console.log(typeof string)
+
+//Boolean
+let boolean = 1 < 2;
+
+console.log(typeof boolean)
+
+//Undefined
+let undefined;
+
+console.log(typeof undefined)
+
+//NaN
+let nan;
+
+console.log(nan + 3)
+
+//Infinity
+let infinity = 4 / 0;
+
+console.log(infinity)
+
+//Bigint
+let myBigInt = BigInt(12345678901234567890);
+
+console.log(typeof myBigInt);
+
+//Symbol
+let mySymbol = Symbol("опис символу");
+
+console.log(typeof mySymbol);
+
+//Object
+let object = {
+    a : 123,
+    b : 321
+};
+
+console.log(typeof object)
+
+//Null
+let Null = null
+
+console.log (typeof Null,Null)
